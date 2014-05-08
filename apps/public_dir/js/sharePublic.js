7de57479ebbb3d8767a8ad73cc08c6f8c681cef4
John Anderson <sontek@gmail.com>
2014-05-08 10:53:15 +0800

    共享至资料库菜单雏形

--- apps/public_dir/js/sharePublic.js
+++ apps/public_dir/js/sharePublic.js
@@ -3,8 +3,38 @@
  */
 
 var PublicShare = {
-    showDropDown:function (itemType, appendTo) {
-        var html = '<div id="dropdown" class="drop" data-item-type="'+itemType+'" data-item-source="'+itemSource+'">';
+    dropdownshown:false,
+    showDropDown:function (itemType, appendTo, src_dir) {
+
+        if (this.dropdownshown === true) {
+            $('div#dropdown').remove();
+            this.dropdownshown = false;
+            return;
+        }
+
+        var html = '<div id="dropdown" class="drop" data-item-type="'
+            +itemType+'" data-item-dir="'+src_dir+'">';
+        var dropDownEl;
+
+        html += '<label for="data-src-dir">目标文件夹</label>';
+        html += '</br>';
+        html += '<input id="data-src-dir" type="text" />';
+        html += '<img id="dir-toggle" class="svg" src="/core/img/actions/caret.png"/>';
+
+        html += '<br/>';
+        html += '<textarea style="display: none" id="dir-selecter"></textarea>';
+        html += '<button type="submit">共享至资料库</button>';
+
+        html += '</div>';
+
+        dropDownEl = $(html);
+        dropDownEl.appendTo(appendTo);
+
+        $('#dir-toggle').click(function(){
+            $('#dir-selecter').toggle();
+        })
+
+        this.dropdownshown = true;
     }
 }
 
@@ -21,7 +51,20 @@ $(document).ready(function (){
             OC.PERMISSION_READ,
             OC.imagePath('core', 'actions/share'),
             function(filename) {
+                if ($('#dir').val() == '/') {
+                    var src_dir = $('#dir').val() + filename;
+                } else {
+                    var src_dir = $('#dir').val() + '/' + filename;
+                }
+                var tr = FileList.findFileEl(filename);
+                if ($(tr).data('type') == 'dir') {
+                    var itemType = 'folder';
+                } else {
+                    var itemType = 'file';
+                }
+                var appendTo = $(tr).find('td.filename');
 
+                PublicShare.showDropDown(itemType, appendTo, src_dir);
             }
         )
     }
